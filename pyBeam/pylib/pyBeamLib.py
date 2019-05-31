595a7f78ed8b99a04ee955cf94eaf4ee2faf538e
Ruben Sanchez <ruben.sanchez@scicomp.uni-kl.de>
2019-05-31 10:25:20 +0200

    Add pyBeam interface for simpler calls, and options to define the linear solver.

---
+++ pyBeam/pylib/pyBeamLib.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+#
+# pyBeam, a Beam Solver
+#
+# Copyright (C) 2018 Ruben Sanchez, Rocco Bombardieri, Rauno Cavallaro
+# 
+# File developers: Ruben Sanchez (SciComp, TU Kaiserslautern)
+#                  Rocco Bombardieri (Carlos III University Madrid)
+#
+# This file is part of pyBeam.
+#
+# pyBeam is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# pyBeam is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero
+# General Public License along with pyBeam.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+# Imports from pyBeam
+from pyBeamIO import pyBeamConfig as pyConfig
+from pyBeamIO import pyBeamInput as pyInput
+import numpy as np
+import pyBeam
+
+# ----------------------------------------------------------------------
+#  Beam object
+# ----------------------------------------------------------------------
+
+class pyBeamSolver:
+  """Description"""
+
+  def __init__(self, config_fileName):
+    """ Description. """
+
+    self.Config_file = config_fileName
+    self.Config = {}
+    
+    print("\n---------------------------------------------------------------------------")
+    print("|                                                                         |")
+    print("| pyBeam, a Beam Solver - Release 0.1 (beta)                              |")
+    print("|                       - https://github.com/rsanfer/pyBeam               |")
+    print("|                                                                         |")
+    print("---------------------------------------------------------------------------")
+    print("|                                                                         |")
+    print("| Copyright 2018-2019 Rocco Bombardieri (Carlos III University Madrid)    |")
+    print("|                     Rauno Cavallaro (Carlos III University Madrid)      |")
+    print("|                     Tim Albring (SciComp, TU Kaiserslautern)            |")
+    print("|                     Ruben Sanchez (SciComp, TU Kaiserslautern)          |")
+    print("|                                                                         |")
+    print("| pyBeam is free software: you can redistribute it and/or                 |")
+    print("| modify it under the terms of the GNU Affero General Public License      |")
+    print("| as published by the Free Software Foundation, either version 3 of the   |") 
+    print("| License, or (at your option) any later version.                         |")
+    print("|                                                                         |")
+    print("| pyBeam is distributed in the hope that it will be useful,               |")
+    print("| but WITHOUT ANY WARRANTY; without even the implied warranty             |")
+    print("| of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 |")
+    print("| See the GNU Affero General Public License for more details.             |")
+    print("|                                                                         |")
+    print("| You should have received a copy of the GNU Affero                       |")
+    print("| General Public License along with pyBeam.                               |")
+    print("| If not, see <http://www.gnu.org/licenses/>.                             |")
+    print("|                                                                         |")
+    print("---------------------------------------------------------------------------\n")
+    
+    # Parsing config file
+    self.Config = pyConfig.pyBeamConfig(config_fileName)  # Beam configuration file
+
+    self.Mesh_file = self.Config['B_MESH']
+    self.Property = self.Config['B_PROPERTY']
+
+    # Parsing mesh file
+    self.nDim= pyInput.readDimension(self.Config['B_MESH'])
+    self.node_py, self.nPoint = pyInput.readMesh(self.Config['B_MESH'], self.nDim)
+    self.elem_py, self.nElem = pyInput.readConnectivity(self.Config['B_MESH'])
+    self.Constr, self.nConstr = pyInput.readConstr(self.Config['B_MESH'])
+    self.RBE2_py, self.nRBE2 = pyInput.readRBE2(self.Config['B_MESH'])
+    # Parsing Property file
+    self.Prop, self.nProp = pyInput.readProp(self.Config['B_PROPERTY'])
+
+    # Initializing objects
+    self.beam = pyBeam.CBeamSolver()
+    self.inputs = pyBeam.CInput(self.nPoint, self.nElem, self.nRBE2)
+
+    # Sending to CInput object
+    pyConfig.parseInput(self.Config, self.inputs, self.Constr, self.nConstr)
+    # Assigning input values to the input object in C++
+    self.inputs.SetParameters()
+    # Initialize the input in the beam solver
+    self.beam.InitializeInput(self.inputs)
+
+    # Assigning values to the CNode objects in C++
+    self.node = []
+    for i in range(self.nPoint):
+        self.node.append(pyBeam.CNode(self.node_py[i].GetID()))
+        for j in range(self.nDim):
+            self.node[i].SetCoordinate(j, float(self.node_py[i].GetCoord()[j][0]))
+            self.node[i].SetCoordinate0(j, float(self.node_py[i].GetCoord0()[j][0]))
+        self.beam.InitializeNode(self.node[i], i)
+
+    # Assigning property values to the property objects in C++
+    self.beam_prop = []
+    for i in range(self.nProp):
+        self.beam_prop.append(pyBeam.CProperty(i))
+        self.beam_prop[i].SetSectionProperties(self.Prop[i].GetA(), self.Prop[i].GetIyy(), self.Prop[i].GetIzz(), self.Prop[i].GetJt())
+
+    # Assigning element values to the element objects in C++
+    self.element = []
+    for i in range(self.nElem):
+        self.element.append(pyBeam.CElement(i))
+        self.element[i].Initializer(self.node[self.elem_py[i].GetNodes()[0, 0] - 1], self.node[self.elem_py[i].GetNodes()[1, 0] - 1],
+                               self.beam_prop[self.elem_py[i].GetProperty() - 1], self.inputs, self.elem_py[i].GetAuxVector()[0, 0],
+                               self.elem_py[i].GetAuxVector()[1, 0], self.elem_py[i].GetAuxVector()[2, 0])
+        self.beam.InitializeElement(self.element[i], i)
+
+    # Here we need to pass the AeroPoint matrix of the wing grid
+    # IF ANY, assigning RBE2_element values to the RBE2 objects in C++
+    if self.nRBE2 != 0:
+        self.RBE2 = []
+        for i in range(self.nRBE2):
+            self.RBE2.append(pyBeam.CRBE2(i))
+            self.RBE2[i].Initializer(self.node[self.RBE2_py[i].GetNodes()[0, 0] - 1], self.node[self.RBE2_py[i].GetNodes()[1, 0] - 1])
+            self.beam.InitializeRBE2(self.RBE2[i], i)
+            
+            
+    # Initialize structures to store the coordinates and displacements
+    
+    self.coordinate_X = []
+    self.coordinate_Y = []
+    self.coordinate_Z = []
+
+    self.coordinate_X0 = []
+    self.coordinate_Y0 = []
+    self.coordinate_Z0 = []
+    
+    self.displacement_X = []
+    self.displacement_Y = []
+    self.displacement_Z = []    
+
+    # finally intializing the structure for the solver
+    self.beam.InitializeStructure()
+
+    print("--> Initialization successful")
+    print("\n---------------------------------------------------------------------------\n")
+
+
+  def SetLoads(self, iVertex, loadX, loadY, loadZ):
+
+    """ This function sets the load  """
+    self.beam.SetLoads(iVertex, 0, loadX)
+    self.beam.SetLoads(iVertex, 1, loadY)
+    self.beam.SetLoads(iVertex, 2, loadZ)
+
+  def GetInitialCoordinates(self,iVertex):
+
+    """ This function returns the initial coordinates of the structural beam model  """
+    coordX = self.beam.ExtractCoordinate0(iVertex, 0)
+    coordY = self.beam.ExtractCoordinate0(iVertex, 1)
+    coordZ = self.beam.ExtractCoordinate0(iVertex, 2)
+
+    return coordX, coordY, coordZ
+
+  def ExtractDisplacements(self,iVertex):
+
+    """ This function returns the initial coordinates of the structural beam model  """
+    dispX = self.beam.ExtractDisplacements(iVertex, 0)
+    dispY = self.beam.ExtractDisplacements(iVertex, 1)
+    dispZ = self.beam.ExtractDisplacements(iVertex, 2)
+
+    return dispX, dispY, dispZ
+
+  def Run(self):
+    """ This function runs the solver and stores the results.
+        Needs to be run after __SetLoads """
+    
+    self.beam.Solve(0)
+    
+    self.coordinate_X = []
+    self.coordinate_Y = []
+    self.coordinate_Z = []
+
+    self.coordinate_X0 = []
+    self.coordinate_Y0 = []
+    self.coordinate_Z0 = []
+    
+    self.displacement_X = []
+    self.displacement_Y = []
+    self.displacement_Z = []    
+
+    for jNode in range(0,self.nPoint):
+    
+        self.coordinate_X.append(self.beam.ExtractCoordinate(jNode, 0))
+        self.coordinate_Y.append(self.beam.ExtractCoordinate(jNode, 1))
+        self.coordinate_Z.append(self.beam.ExtractCoordinate(jNode, 2))  
+  
+        self.coordinate_X0.append(self.beam.ExtractCoordinate0(jNode, 0))
+        self.coordinate_Y0.append(self.beam.ExtractCoordinate0(jNode, 1))
+        self.coordinate_Z0.append(self.beam.ExtractCoordinate0(jNode, 2))
+        
+        self.displacement_X.append(self.beam.ExtractDisplacements(jNode, 0))
+        self.displacement_Y.append(self.beam.ExtractDisplacements(jNode, 1))
+        self.displacement_Z.append(self.beam.ExtractDisplacements(jNode, 2))        
+
+  def PrintDisplacements(self, iVertex):
+    
+    """ This function prints to screen the displacements on the nodes """
+    print("\n--> Coord0({}): {:16.12f} {:16.12f} {:16.12f}".format(iVertex, 
+                                      self.coordinate_X0[iVertex], 
+                                      self.coordinate_Y0[iVertex], 
+                                      self.coordinate_Z0[iVertex]))
+                                      
+    print("--> Coord({}) : {:16.12f} {:16.12f} {:16.12f}".format(iVertex, 
+                                      self.coordinate_X[iVertex], 
+                                      self.coordinate_Y[iVertex], 
+                                      self.coordinate_Z[iVertex]))  
+  
+    print("--> Displ({}) : {:16.12f} {:16.12f} {:16.12f}\n".format(iVertex,
+                                      self.displacement_X[iVertex], 
+                                      self.displacement_Y[iVertex], 
+                                      self.displacement_Z[iVertex]))
+                                      
+  def TestNodePosition(self, iVertex, tol, coorX, coorY, coorZ):
+    
+    test_val = np.sqrt((self.coordinate_X[20]-coorX)**2+
+                       (self.coordinate_Y[20]-coorY)**2+
+                       (self.coordinate_Z[20]-coorZ)**2)
+    
+    # Tolerance is set to 1E-8
+    if (test_val < tol):
+      print("--> Tolerance: {:16.12E} (< {:16.12E})".format(test_val, tol))
+      return(0)
+    else:
+      print("--> Tolerance: {:16.12E} (> {:16.12E})".format(test_val, tol))      
+      print("--> TEST FAILED")           
+      return(1)
+
+
+
+
+
+
+
+
+
+
+
--- tests/regression/regression.py
+++ tests/regression/regression.py
@@ -24,131 +24,16 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-import sys, os
-from pyBeamIO import pyBeamConfig as pyConfig
-from pyBeamIO import pyBeamInput as pyInput
-import pyBeam
+from pyBeamLib import pyBeamSolver
 
-# Load running directory
-rundir = os.path.dirname(os.path.realpath(__file__))
-confFile = rundir + '/config.cfg'
+beam = pyBeamSolver('config.cfg')
 
-# Parsing Conf file
-config = pyConfig.pyBeamConfig(confFile)  # Beam configuration file
+beam.SetLoads(20,0,5000,1000)
 
-# Specifically added for the test
-config['B_PROPERTY'] = rundir + '/' + config['B_PROPERTY'][:]
-config['B_MESH'] = rundir + '/' + config['B_MESH'][:]
+beam.Run()
 
-# Parsing mesh file
-nDim = pyInput.readDimension(config['B_MESH'])
-node_py, nPoint = pyInput.readMesh(config['B_MESH'],nDim)
-elem_py, nElem = pyInput.readConnectivity( config['B_MESH'])
-Constr, nConstr = pyInput.readConstr(config['B_MESH'])
-# Parsing Property file
-Prop, nProp = pyInput.readProp(config['B_PROPERTY'])
+beam.PrintDisplacements(20)
 
-# Initializing objects
-beam = pyBeam.CBeamSolver()
-inputs = pyBeam.CInput(nPoint, nElem)
+success = beam.TestNodePosition(20,1e-8,24.0199395394,16.2954533731,0.389860802844)
 
-# Sending to CInput object 
-pyConfig.parseInput(config, inputs, Constr, nConstr)
-# Assigning input values to the input object in C++
-inputs.SetParameters()
-# Initialize the input in the beam solver
-beam.InitializeInput(inputs)
-
-# Assigning values to the CNode objects in C++
-node = []  
-for i in range(nPoint):
-   node.append( pyBeam.CNode(node_py[i].GetID()) )
-   for j in range(nDim):
-      node[i].SetCoordinate(j , float(node_py[i].GetCoord()[j][0]) )
-      node[i].SetCoordinate0(j , float(node_py[i].GetCoord0()[j][0]) )
-   beam.InitializeNode(node[i], i)
-    
-# Assigning property values to the property objects in C++
-beam_prop = []
-for i in range(nProp):
-    beam_prop.append(pyBeam.CProperty(i))
-    beam_prop[i].SetSectionProperties( Prop[i].GetA(),  Prop[i].GetIyy(),  Prop[i].GetIzz(),  Prop[i].GetJt())
-  
-# Assigning element values to the property objects in C++ 
-element =[]
-for i in range(nElem): 
-   element.append(pyBeam.CElement(i))
-   #element[i].Initializer(CNode* Node1, CNode* Node2, CProperty* Property, CInput* Input, addouble AuxVector_x, addouble AuxVector_y, addouble AuxVector_z)
-   #NB node starts from index 0 and the same happen in beam_prop. But in element_py (connectivity) indexes start from 1 as it is the physical connectivity read from input file
-   element[i].Initializer(node[elem_py[i].GetNodes()[0,0] -1], node[elem_py[i].GetNodes()[1,0] -1], beam_prop[elem_py[i].GetProperty() -1], inputs, elem_py[i].GetAuxVector()[0,0], elem_py[i].GetAuxVector()[1,0], elem_py[i].GetAuxVector()[2,0]  )
-   beam.InitializeElement(element[i], i)
-  
-beam.InitializeStructure()
-
-iNode = 21   -1
-beam.SetLoads(iNode,1,5000)
-beam.SetLoads(iNode,2,1000)
-beam.Solve(0)
-
-coordinate_X = []
-coordinate_Y = []
-coordinate_Z = []
-
-coordinate_X0 = []
-coordinate_Y0 = []
-coordinate_Z0 = []
-
-for jNode in range(0,nPoint):
-    
-  coordinate_X.append(beam.ExtractCoordinate(jNode, 0))
-  coordinate_Y.append(beam.ExtractCoordinate(jNode, 1))
-  coordinate_Z.append(beam.ExtractCoordinate(jNode, 2))  
-  
-  coordinate_X0.append(beam.ExtractCoordinate0(jNode, 0))
-  coordinate_Y0.append(beam.ExtractCoordinate0(jNode, 1))
-  coordinate_Z0.append(beam.ExtractCoordinate0(jNode, 2))
-  
-#for iNode in range(0,nPoint):  
-print("Node {} Coord_tip indef= {} {} {}".format(iNode, coordinate_X0[iNode], coordinate_Y0[iNode], coordinate_Z0[iNode]))  
-  
-  
-print("Coord_tip= {} {} {}".format(coordinate_X[iNode], coordinate_Y[iNode], coordinate_Z[iNode]))  
-  
-print("Displ_tip= {} {} {}".format(coordinate_X[iNode] - coordinate_X0[iNode], coordinate_Y[iNode] - coordinate_Y0[iNode], coordinate_Z[iNode] - coordinate_Z0[iNode]))  
-
-'''  
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-  
-# Create cubic bounding box to simulate equal aspect ratio
-max_range = np.array([np.amax(coordinate_X0)-np.amin(coordinate_X0), np.amax(coordinate_Y0)-np.amin(coordinate_Y0), np.amax(coordinate_Z0)-np.amin(coordinate_Z0)]).max()
-Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(np.amax(coordinate_X0)+np.amin(coordinate_X0))
-Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(np.amax(coordinate_Y0)+np.amin(coordinate_Y0))
-Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(np.amax(coordinate_Z0)+np.amin(coordinate_Z0))
-# Comment or uncomment following both lines to test the fake bounding box:
-for xb, yb, zb in zip(Xb, Yb, Zb):
-   plt.plot([xb], [yb], [zb], 'w')  
-  
-plt.plot(coordinate_X, coordinate_Y, coordinate_Z)
-plt.plot(coordinate_X0, coordinate_Y0, coordinate_Z0)
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()
-'''  
-  
-
-test_val = np.sqrt((coordinate_X[20]-24.0199395394)**2+
-                   (coordinate_Y[20]-16.2954533731)**2+
-                   (coordinate_Z[20]-0.389860802844)**2)
-
-
-print("Tolerance: ",test_val)
-
-# Tolerance is set to 1E-8
-if (test_val < 1e-8):
-  exit(0)
-else:
-  exit(1)
+exit(success)
